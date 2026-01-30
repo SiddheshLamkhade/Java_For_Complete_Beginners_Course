29124b5e745ca89c59b6eb09aad36282ab96befb
siddhesh.lamkhade_co <sidlamkhade@gmail.com>
2026-01-30 13:09:31 +0530

    new changes are added

--- Inheritance.java
+++ Inheritance.java
@@ -1,23 +1,33 @@
 class Week1 {
-    Week1(){
-        System.out.println("this is constructor of week1");
-    }
+//    Week1(){
+//        System.out.println("this is constructor of week1");
+//    }
     public void show(){
         System.out.println("this is class parent");
     }
 }
 class Monday extends  Week1{
-    Monday(){
-        super();
-    }
+//    Monday(){
+//        super();
+//    }
     public void show(){
         System.out.println("this is class child");
+        //super.show();
+    }
+}
+class Mondayhour1 extends  Monday{
+    //    Monday(){
+//        super();
+//    }
+    public void show(){
+        System.out.println("this is class Mondayhour1");
         super.show();
     }
 }
+
 public class Inheritance{
     public static void main() {
-        Week1 obj2= new Monday();
+        Week1 obj2= new Mondayhour1();
         obj2.show();
 
 
---
+++ ScopesTopic1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class ScopesTopic1 {
+
+
+    private int a;
+     //Instance variable   inside a class but outside methods
+    // Default values are provided (0, null, false)
+
+    static int b;
+    //Class Variables-Static Variables
+    // Default values are provided (0, null, false)
+
+    public static void main() {
+        Scanner sc= new Scanner(System.in);
+        int c=23;
+        //local variable Must be initialized before use
+        System.out.println(b);
+
+    }
+}
