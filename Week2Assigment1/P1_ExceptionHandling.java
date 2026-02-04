26d45fe5fd659d2203c05a59d8cd42eee757b56d
siddhesh.lamkhade_co <sidlamkhade@gmail.com>
2026-02-04 14:07:55 +0530

    added the solution of problem no. 1

--- Week2Assigment1/P1_ExceptionHandling.java
+++ Week2Assigment1/P1_ExceptionHandling.java
@@ -1,4 +1,30 @@
 package Week2Assigment1;
+import java.util.*;
 
 public class P1_ExceptionHandling {
+    static void main() {
+        Scanner input=new Scanner(System.in);
+
+
+            try {
+                System.out.print("Enter first number : ");
+                int num1=input.nextInt();
+                System.out.print("Enter second number : ");
+                int num2= input.nextInt();
+                int c= num1/num2;
+                System.out.println("Result is : "+c);
+            }
+            catch (ArithmeticException e){
+                System.out.println("ArithmeticException has been occured");
+            }
+            catch (InputMismatchException ie){
+                System.out.println("InputMismatchException has been occured");
+            }
+
+            finally {
+                System.out.println("Program execution completed");
+            }
+
+    }
+
 }
