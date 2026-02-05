31ce17774e14a28560db68556cb50b54c040c0ba
siddhesh.lamkhade_co <sidlamkhade@gmail.com>
2026-02-05 13:19:39 +0530

    adding problem no. 6

---
+++ Week2Assigment1/Problem6/UseCaseOfLambda.java
@@ -0,0 +1,28 @@
+package Week2Assigment1.Problem6;
+import  java.util.*;
+public class UseCaseOfLambda {
+    public static void main() {
+        List<String> employees = new ArrayList<>();
+        // We have been asked to create list of String that's why I haven't created list of employee objects
+        employees .add("Siddhesh");
+        employees .add("Ram");
+        employees .add("Sita");
+        employees .add("Snehal");
+
+        System.out.println("\nAll Names : ");
+        // printing names using lambda function as asked
+        employees.forEach(e -> System.out.println(e));
+
+        System.out.println("\nNames starting with 'S':");
+        // Filtering names starting with 'A'
+        employees.stream()
+                .filter(e -> e.startsWith("S"))
+                .forEach(name -> System.out.println(name));
+
+        System.out.println("\nNames in alphabetical order:");
+        // Sorting names alphabetically
+        employees.stream()
+                .sorted()
+                .forEach(name -> System.out.println(name));
+    }
+}
